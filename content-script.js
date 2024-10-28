780822f5a4a5633b489fa4780af2f2b413f3a979
rich <h43z@users.noreply.github.com>
2024-10-28 20:47:27 +0100

    rewrite

--- content-script.js
+++ content-script.js
@@ -1,26 +1,50 @@
-const originalAudioContext = AudioContext
-const originalCreateBufferSource = AudioContext.prototype.createBufferSource
+(() => {
 
-let suspendTimer = null
-const autoSuspend = ctx => {
-  ctx.resume()
+  if(localStorage.getItem('disableAudioContextSuspender'))
+    return
 
-  if(suspendTimer)
-    clearTimeout(suspendTimer)
+  const originalAudioContext = AudioContext
+  const originalCreateBufferSource = AudioContext.prototype.createBufferSource
+  const map = new Map()
 
-  suspendTimer = setTimeout(_ => {
-    ctx.suspend().then(_=>console.log('Auto suspended AudioContext', ctx))
-    suspendTimer = null
-  }, 2000)
-}
+  const autoSuspend = ctx => {
+    const timer = map.get(ctx)
 
-AudioContext.prototype.createBufferSource = function(){
-  autoSuspend(this)
-  return originalCreateBufferSource.call(this)
-}
+    if(timer)
+      clearTimeout(timer)
 
-AudioContext = function(){
-  const ctx = new originalAudioContext()
-  autoSuspend(ctx)
-  return ctx
-}
+    map.set(ctx, setTimeout(function(){
+      map.delete(ctx)
+
+      if(ctx.state === 'suspended')
+        return
+
+      ctx.suspend().then(_=>console.log('Auto-suspended AudioContext'))
+    }, 2000))
+  }
+
+  AudioContext.prototype.createBufferSource = function(){
+    const buffer = originalCreateBufferSource.call(this)
+    buffer._audioContext = this
+
+    buffer.start = function(){
+      autoSuspend(this._audioContext)
+
+      if(this._audioContext.state === 'running')
+        return AudioScheduledSourceNode.prototype.start.call(this)
+
+      this._audioContext.resume().then(_=>{
+        console.log('Resumed auto-suspended AudioContext')
+        return AudioScheduledSourceNode.prototype.start.call(this)
+      })
+    }
+
+    return buffer
+  }
+
+  AudioContext = function(){
+    const ctx = new originalAudioContext()
+    autoSuspend(ctx)
+    return ctx
+  }
+})();
